6e93f88bc3a03ba8cf1aa8ddf845576025f63927
Viacheslav Alekseev <VA_Alekseev@outlook.com>
2022-03-28 13:21:31 +0300

    3

--- CopyGroupPlugin/Class1.cs
+++ CopyGroupPlugin/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
@@ -15,22 +16,113 @@ namespace CopyGroupPlugin
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
-            Document doc = uiDoc.Document;
+            try
+            {
+                UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+                Document doc = uiDoc.Document;
+                GroupPickFilter groupPickFilter = new GroupPickFilter();
+                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, groupPickFilter, "Выберите группу объектов");
+                Element element = doc.GetElement(reference);
+                Group group = element as Group;
+                XYZ groupCentr = GetElementCenter(element);
+                Room room = GetRoomByPoin(doc, groupCentr);
+                XYZ roomCentr = GetElementCenter(room);
+                XYZ offset = groupCentr - roomCentr;
 
-            Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, "Выберите группу объектов");
-            Element element = doc.GetElement(reference);
-            Group group = element as Group;
+                RoomPickFilter roomPickFilter = new RoomPickFilter();
+                IList<Reference> rooms = uiDoc.Selection.PickObjects(ObjectType.Element, roomPickFilter,
+                "Выберите комнаты, в которые требуется скопировать группу мебели");
 
-            XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
+                XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
 
-            Transaction transaction = new Transaction(doc);
-            transaction.Start("Копирование группы объектов");
+                Transaction transaction = new Transaction(doc);
+                transaction.Start("Копирование группы объектов");
 
-            doc.Create.PlaceGroup(point, group.GroupType);
-            transaction.Commit();
+                PlaceFurnitureInRooms(doc, rooms, group.GroupType, offset);
+
+                transaction.Commit();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
 
             return Result.Succeeded;
+        }
+
+        public XYZ GetElementCenter(Element element)
+        {
+            BoundingBoxXYZ bounding = element.get_BoundingBox(null);
+            return (bounding.Max + bounding.Min) / 2;
+        }
+        public Room GetRoomByPoin(Document doc, XYZ poin)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (Element e in collector)
+            {
+                Room room = e as Room;
+                if (room != null)
+                {
+                    if (room.IsPointInRoom(poin))
+                    {
+                        return room;
+                    }
+                }
+            }
+            return null;
+        }
+        public void PlaceFurnitureInRooms(Document doc, IList<Reference> rooms, GroupType gt, XYZ offsetXY)
+        {
+
+            //XYZ offsetXY = new XYZ(offset.X, offset.Y, 0);
+            foreach (Reference r in rooms)
+            {
+                Room roomTarget = doc.GetElement(r) as Room;
+                if (roomTarget != null)
+                {
+                    XYZ roomCenter = GetElementCenter(roomTarget);
+                    Group group = doc.Create.PlaceGroup(roomCenter + offsetXY, gt);
+                }
+            }
+        }
+
+        public class GroupPickFilter : ISelectionFilter
+        {
+            public bool AllowElement(Element elem)
+            {
+                if (elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_IOSModelGroups)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public bool AllowReference(Reference reference, XYZ position)
+            {
+                return false;
+            }
+        }
+        public class RoomPickFilter : ISelectionFilter
+        {
+            public bool AllowElement(Element e)
+            {
+                return (e.Category.Id.IntegerValue.Equals(
+                (int)BuiltInCategory.OST_Rooms));
+            }
+            public bool AllowReference(Reference r, XYZ p)
+            {
+                return false;
+            }
         }
     }
 }
